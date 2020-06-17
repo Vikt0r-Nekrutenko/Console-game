ac205f7aa35240a9d65a23b86b6381e9cbf269f5
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2020-06-18 01:49:24 +0300

    base bullet entity

--- main.c
+++ main.c
@@ -2,10 +2,37 @@
 #include "renderer.h"
 #include "player.h"
 
+#define CONSOLE_BULLET_DEF_VELOCITY 0.01f
+
+struct ConsoleBullet {
+    float px, py;
+    float dx, dy;
+    BOOL isActive;
+} bullet;
+
+void ConsoleBulletUpdate(ConsoleRenderer *rend, const float deltaTime)
+{
+    if (bullet.isActive == TRUE) {
+        if (bullet.dy < bullet.py) {
+            bullet.py -= CONSOLE_BULLET_DEF_VELOCITY;
+        }
+        if (bullet.dy > bullet.py) {
+            bullet.py += CONSOLE_BULLET_DEF_VELOCITY;
+        }
+        if (bullet.dx < bullet.px) {
+            bullet.px -= CONSOLE_BULLET_DEF_VELOCITY;
+        }
+        if (bullet.dx > bullet.px) {
+            bullet.px += CONSOLE_BULLET_DEF_VELOCITY;
+        }
+        ConsoleRendererPutPixel(rend, bullet.px, bullet.py, 7, FG_YELLOW);
+    }
+}
 
 void ConsoleWindowUpdateProc(ConsoleWindow *wnd, const float deltaTime)
 {
     ConsolePlayerUpdate(wnd->_rend);
+    ConsoleBulletUpdate(wnd->_rend, deltaTime);
 }
 
 void ConsoleWindowKeyEventProc(ConsoleWindow *wnd, const KEY_EVENT_RECORD *ker)
@@ -26,7 +53,11 @@ void ConsoleWindowKeyEventProc(ConsoleWindow *wnd, const KEY_EVENT_RECORD *ker)
 
 void ConsoleWindowMouseEventProc(ConsoleWindow *wnd, const MOUSE_EVENT_RECORD *mer)
 {
-
+    if (mer->dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
+        bullet.px = player._px; bullet.py = player._py;
+        bullet.dx = mer->dwMousePosition.X; bullet.dy = mer->dwMousePosition.Y;
+        bullet.isActive = TRUE;
+    }
 }
 
 int main()
